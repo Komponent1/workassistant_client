b891b82fc6134dbf0a1ba65e9c8cadc86d0b2942
seo2im <seo2im6492@gmail.com>
2022-12-11 14:48:35 +0900

    🎸 codepush stage 테스트(ios)

--- App.tsx
+++ App.tsx
@@ -11,8 +11,18 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import Config from 'react-native-config';
+import codePush from 'react-native-code-push';
 
-console.log(Config);
+const codePushOptions = {
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+  // 언제 업데이트를 체크하고 반영할지를 정한다.
+  // ON_APP_RESUME은 Background에서 Foreground로 오는 것을 의미
+  // ON_APP_START은 앱이 실행되는(켜지는) 순간을 의미
+  updateDialog: false,
+  // 업데이트를 할지 안할지 여부에 대한 노출 (잠수함 패치의 경우 false)
+  installMode: codePush.InstallMode.IMMEDIATE,
+  // 업데이트를 어떻게 설치할 것인지 (IMMEDIATE는 강제설치를 의미)
+};
 
 const App = () => {
   return (
@@ -22,4 +32,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default codePush(codePushOptions)(App);
